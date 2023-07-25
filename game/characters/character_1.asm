b2a75edb661cffbb5b971c68f63e53ff22809775
Mena Tawadros <mena2871@gmail.com>
2023-07-24 22:29:13 -0700

    Add sprite requests for characters

--- game/characters/character_1.asm
+++ game/characters/character_1.asm
@@ -1,13 +1,15 @@
-.section "Character_1" BANK 0 SLOT "ROM"
-
 .struct Character_1
     id         db ; Character ID
+    sprite_ptr dw ; Sprite pointer
     character_attr instanceof Character_Attr
 .endst
 
 .enum $0000
     character_1 instanceof Character_1
 .ende
+
+.section "Character_1" BANK 0 SLOT "ROM"
+nop
 
 Character_1_Init:
     phy
@@ -19,7 +21,34 @@ Character_1_Init:
     lda #3
     sta character_1.character_attr.speed, X
     A16
-    
+
+    ; Load a sprite
+    phx
+    jsr SpriteManager_Request
+    lda #Sprite_Plane@Bank
+    ldx #Sprite_Plane@Data
+    jsr Sprite_Load 
+    plx
+
+    ; Save pointer to the sprite
+    sty character_1.sprite_ptr.w, X
+
+    A8
+    lda #50
+    sta sprite_desc.x, Y
+
+    lda #150
+    sta sprite_desc.y, Y
+    A16
+
+    ; Set the tag of the sprite to the Forward animation
+    lda #Sprite_Plane@Tag@Forward
+    jsr Sprite_SetTag
+
+    ; Set the frame of the sprite to 0
+    lda #0
+    jsr Sprite_SetFrame
+
     ply
     rts
 
--- game/characters/character_2.asm
+++ game/characters/character_2.asm
@@ -1,13 +1,15 @@
-.section "Character_2" BANK 0 SLOT "ROM"
-
 .struct Character_2
     id         db ; Character ID
+    sprite_ptr dw ; Sprite pointer
     character_attr instanceof Character_Attr
 .endst
 
 .enum $0000
     character_2 instanceof Character_2
 .ende
+
+.section "Character_2" BANK 0 SLOT "ROM"
+nop
 
 Character_2_Init:
     phy
@@ -19,6 +21,33 @@ Character_2_Init:
     lda #1
     sta character_2.character_attr.speed, X
     A16
+
+    ; Load a sprite
+    phx
+    jsr SpriteManager_Request
+    lda #Sprite_Plane@Bank
+    ldx #Sprite_Plane@Data
+    jsr Sprite_Load 
+    plx
+
+    ; Save pointer to the sprite
+    sty character_2.sprite_ptr.w
+
+    A8
+    lda #50
+    sta sprite_desc.x, Y
+
+    lda #100
+    sta sprite_desc.y, Y
+    A16
+
+    ; Set the tag of the sprite to the Forward animation
+    lda #Sprite_Plane@Tag@Forward
+    jsr Sprite_SetTag
+
+    ; Set the frame of the sprite to 0
+    lda #0
+    jsr Sprite_SetFrame
 
     ply
     rts
